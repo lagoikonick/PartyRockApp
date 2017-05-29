96a8268800ff644e5c992286c7f1e58826ed0a00
Lagoiko Nick <lagoiko.nick@gmail.com>
2017-05-29 10:25:45 +0300

    added some classes

--- PartyRockApp/PartyCell.swift
+++ PartyRockApp/PartyCell.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class PartyCell: UITableViewCell {
 
+    
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,8 +23,13 @@ class PartyCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
+    
+    func updateUI (partyRock: PartyRock) {
+        videoTitle.text = partyRock.videoTitle
+        //TODO: set image from URL
+    }
+    
+    
 }
