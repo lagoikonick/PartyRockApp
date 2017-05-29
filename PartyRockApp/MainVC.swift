acce5c1c884f894ae488e302d2be09244270ae41
Lagoiko Nick <lagoiko.nick@gmail.com>
2017-05-29 09:45:05 +0300

    added interface
    main VC UI
    segue
    added new class

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Nick on 29.05.17.
+//  Copyright © 2017 LagoikoNick. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+  
+}
+
---
+++ PartyRockApp/PartyCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Nick on 29.05.17.
+//  Copyright © 2017 LagoikoNick. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
